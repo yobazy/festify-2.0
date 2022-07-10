a38ebdf5bb56c67892ff05976d7db2470eeff3ba
Bazil <king.bazil.khan@gmail.com>
2022-07-10 02:12:26 +0000

    Adds props for testing

--- client/src/components/Event/index.js
+++ client/src/components/Event/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 
 export default function Event(props)  {
   return (
-    <article className="event">Event</article>
+    <div>
+      <article className="event">Event</article>
+      <h1>props.eventName</h1>
+      <h1>props.eventDate</h1>
+    </div>
   )
 }
