b3d64eafbf0dc7e4d5e11079c89a78e9465afaa4
Bazil <king.bazil.khan@gmail.com>
2022-07-18 21:58:15 +0000

    Fix createPlaylist method

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,7 +1,8 @@
 require 'dotenv'
 require 'json'
-require "uri"
 require 'rspotify'
+require 'uri'
+require 'net/http'
 
 Dotenv.load
 
@@ -22,49 +23,32 @@ class SpotifyController < ApplicationController
   end
 
   def createPlaylist
-    # client_id = ENV["SPOTIFY_CLIENT_ID"]
-    # client_secret = ENV["SPOTIFY_CLIENT_SECRET"]
-    # RSpotify.authenticate(client_id, client_secret)
-
-    # spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-
-    # puts spotify_user.country
-
-    access_token = ENV["ACCESS_TOKEN"]
-    # auth = {"Authorization": "Bearer #{access_token}"}
-    auth1 = 'Bearer ' + access_token
-
     user_id = getUserID
+    access_token = ENV["ACCESS_TOKEN"]
 
-    data = {
-      "name": "New Playlist",
-      # "description": "New playlist description",
-      "public": true
+
+    url = "https://api.spotify.com/v1/users/#{user_id}/playlists"
+
+    uri = URI.parse(url)
+
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Accept"] = "application/json"
+    request["Authorization"] = "Bearer #{access_token}"
+
+    request.body = JSON.dump({
+      "name" => "Test Playlist",
+      "description" => "New playlist description",
+      "public" => true
+    })
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
     }
- 
-    url = 'https://api.spotify.com/v1/users/'+ user_id +'/playlists'
 
-    puts url
-
-    response = RestClient.post(url, header={Authorization: auth1}, data=data)
-
-    #   {
-    #   "name": "New Playlist",
-    #   # "description": "New playlist description",
-    #   "public": true}
-
-    # url = 'https://api.spotify.com/v1/users/'+user_id+'/playlists'
-
-    # response = RestClient::Request.new({
-    # method: :post,
-    # url: url,
-    # headers: {content_type: 'application/json', Authorization: auth1},
-    # data: data,
-    # dadad: 'dada'
-    # }).execute
-    # end
-    data1 = JSON.parse(response)
-    render json: data1
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
     
   end
 
