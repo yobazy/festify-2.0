3a9c446788ce544d5c69bbacdf8305d94ec2f34f
Bazil <king.bazil.khan@gmail.com>
2022-07-19 01:08:10 +0000

    Clean up spotify controller code

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -84,58 +84,7 @@ class SpotifyController < ApplicationController
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-
   end
-
-
-
-
-
-
-
-
-
-
-
-    # puts 'adding tracks'
-    # uris = ["spotify:track:7IZJ77l62dgOeHwoKzJQTv", "spotify:track:5PEwMWC5koD2OTNblKT6f0"]
-    # playlist_id = "0103aYYzgGTxNnhywPvbQt"
-    
-    # url = "https://api.spotify.com/v1/playlists/#{playlist_id}/tracks?"
-
-    # uri = URI.parse("https://api.spotify.com/v1/playlists/0103aYYzgGTxNnhywPvbQt/tracks?uris=spotify%3Atrack%3A7IZJ77l62dgOeHwoKzJQTv")
-    # request = Net::HTTP::Post.new(uri)
-    # request.content_type = "application/json"
-    # request["Accept"] = "application/json"
-    # request["Authorization"] = "Bearer BQDNlESf80vpnktay2wRT85tng2nXyXK1AyIcHlyujpuMFvOyLlJs3IO2UHp9cbhFj9E6yoq229TVZMpgrIKqbXZt9ACmukBghE23MLOPu7ZjR4-0KHsPzP9tSjlQ8oCHXwyudYb10FLmhZI0M2pCb4QcwiFxrGuwEIJRPuXRPKXS3D2NN88bLH-wzDLNSoKU5kATiuYIBQmtPKzgBLIyMbtukQz0HWpWYmkJhcPazVOsPmC"
-
-    # req_options = {
-    #   use_ssl: uri.scheme == "https",
-    # }
-
-    # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-    #   http.request(request)
-    # end 
-    # # data1 = JSON.parse(resonse)
-    # render json: response
-    # # uri = URI.parse(url)
-    # # request = Net::HTTP::Post.new(uri)
-    # # request.content_type = "application/json"
-    # # request["Authorization"] = ""
-    # # request.body = JSON.dump({
-    # #   "uris" => [
-    # #     "spotify:track:7IZJ77l62dgOeHwoKzJQTv"
-    # #   ],
-    # #   "position" => 0
-    # # })
-
-    # # req_options = {
-    # #   use_ssl: uri.scheme == "https",
-    # # }
-
-    # # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-    # #   http.request(request)
-    # # end
 
   def followPlaylist
   end
