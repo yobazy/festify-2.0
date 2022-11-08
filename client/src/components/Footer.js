64521c9a735ba326dc89d95ce9edb1a486d79382
Baz <king.bazil.khan@gmail.com>
2022-11-08 22:48:59 +0000

    Adds footer styling sheet and updates footer styles

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import './Footer.css'
 
 export default function Footer() {
     return (
-        <div>
-            <p> This is where the footer will go</p>
+        <div className="white pad">
+            <p>Thanks for using Festify!</p>
+            <p>Bazil Khan 2022</p>
         </div>
     )
 }
