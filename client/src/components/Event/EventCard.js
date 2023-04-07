87bceb62182f28ce7ea44a510910c4370427a6c9
Bazil <bazil.khan@urbaneyes.ai>
2023-04-07 19:21:48 -0400

    creates inital event card

---
+++ client/src/components/Event/EventCard.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import './EventCard.css'
+
+const EventCard = ({ imageUrl, eventName, eventLocation }) => {
+  return (
+    <div className="event-card">
+      <img className="event-image" src={imageUrl} alt={eventName} />
+      <div className="event-info">
+        <h3 className="event-name">{eventName}</h3>
+        <p className="event-location">{eventLocation}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
