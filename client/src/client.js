e955f5b012388eaca8159fd8f81b6114b95a64a8
Bazil <king.bazil.khan@gmail.com>
2023-10-06 12:28:58 -0400

    fixes issues with env file not loading properly

--- client/src/client.js
+++ client/src/client.js
@@ -1,6 +1,5 @@
-
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zdbroencbancathizkro.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
