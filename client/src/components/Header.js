ba34d3f02155a919116603d939c64aee5e5c7028
Bazil <king.bazil.khan@gmail.com>
2024-01-04 11:58:54 -0500

    Adds fading animation in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate, Navigate } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../images/icon.png";
@@ -7,11 +7,24 @@ import '../App.css';
 
 
 export default function Header() {
+  const headerWords = ["festivals", "playlists", "artists"];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentWordIndex(currentWordIndex => (currentWordIndex + 1) % headerWords.length);
+    }, 3000); // Change word every 3 seconds
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="header-box">
       <img src={headerImg} className="header-img" alt="header" />
       <div className="header-txt">
-        <h1 className="">Welcome to Festify!</h1>
+        <h1 className="">
+          Discover <span className="rotating-word">{headerWords[currentWordIndex]}</span>
+        </h1>
         <h3 className="">
           Search for a event below. Festify will generate a Spotify playlist
           based on the event's lineup!
