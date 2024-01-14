679ed5c452336b670b436202743cac11abb55c87
Bazil <bazilkhn@gmail.com>
2024-01-14 18:38:19 -0500

    Adds supabase call for event info

--- client/src/routes/Event.js
+++ client/src/routes/Event.js
@@ -6,26 +6,30 @@ import userIcon from '../images/placeholder-user.png'
 import { getEventById, getEventInfo } from '../utils/api'
 import './Event.css';
 
-export default function Event({ events, artists }) {
+export default function Event() {
 
   // get event id from url parameter
   const params = useParams();
   const eventID = params['id']
 
-  const event = events.find((event) => event.event_id == eventID);
-
-// get event information from db
-  const eventInfo = getEventById(eventID);
-
-  console.log("EVENT", eventInfo);
+  // const event = events.find((event) => event.event_id == eventID);
+  const [eventInfo, setEventInfo] = useState(null);
 
 
+  // get event information from db
+  useEffect(() => {
+    // Fetch event information
+    const fetchEvent = async () => {
+      try {
+        const data = await getEventById(eventID);
+        setEventInfo(data);
+      } catch (error) {
+        console.error("Error fetching event data:", error);
+      }
+    };
 
-
-
-
-
-
+    fetchEvent();
+  }, [eventID]); // The effect will run again if eventID changes
 
 
 
@@ -34,25 +38,25 @@ export default function Event({ events, artists }) {
   //filter artists by artistID
   // const eventArtistsArr = event.artists
 
-  let filteredArtists = artists.filter(artist => event.artists.includes(artist.artist_id));
+  // let filteredArtists = artists.filter(artist => event.artists.includes(artist.artist_id));
 
-  let artistCount = filteredArtists.length
+  // let artistCount = filteredArtists.length
 
-  const artistList = filteredArtists.map((artist) => {
-    return artist.name;
-  });
+  // const artistList = filteredArtists.map((artist) => {
+  //   return artist.name;
+  // });
 
   // return artist card with img and name
-  const artistsCard = artistList.map((artist, i) => {
-    return (
-      <div className="artist-card">
-        <img src={userIcon} width="70px"/>
-        <p className="artist-name center" key={i}>
-          {artist}
-        </p>
-      </div>
-    );
-  });
+  // const artistsCard = artistList.map((artist, i) => {
+  //   return (
+  //     <div className="artist-card">
+  //       <img src={userIcon} width="70px"/>
+  //       <p className="artist-name center" key={i}>
+  //         {artist}
+  //       </p>
+  //     </div>
+  //   );
+  // });
 
   return (
     <div>
@@ -70,11 +74,17 @@ export default function Event({ events, artists }) {
         </div> */}
       </div>
       <div className="event-header white border">
-        <h1>{event.name}</h1>
-        <h2>{event.location}</h2>
-        <h2>{event.date}</h2>
+        {eventInfo ? (
+          <>
+        <h1>{eventInfo.event_name}</h1>
+        <h2>{eventInfo.event_location}</h2>
+        <h2>{eventInfo.event_date}</h2>
         {/* <a>{eventLink}</a> */}
         {/* <Artist className="center" tracks={tracks} artist={artist} artistInfo={artistInfo} /> */}
+        </>
+        ) : (
+          <h1></h1>
+        )}
       </div>
       <div>
         <EmbedPlayer src={null} />
@@ -82,9 +92,9 @@ export default function Event({ events, artists }) {
       <div className="center">
         <div className="artists-section-header">
           <h1 className="list-name border">LINEUP</h1>
-          <p className="border">Total Artists: {artistCount}</p>
+          {/* <p className="border">Total Artists: {artistCount}</p> */}
         </div>
-        <div className="center artists-container ">{artistsCard}</div>
+        {/* <div className="center artists-container ">{artistsCard}</div> */}
       </div>
     </div>
   );
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,15 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
-import { config } from 'dotenv'
 
 const SUPABASE_URL = 'https://zdbroencbancathizkro.supabase.co'
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_KEY
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export async function getEventById(eventId) {
+  console.log('env', process.env.REACT_APP_SUPABASE_KEY)
+
   let { data, error } = await supabase
       .from('events')
       .select('*')
-      .eq('id', eventId)
+      .eq('event_id', eventId)
       .single()
 
   if (error) {
