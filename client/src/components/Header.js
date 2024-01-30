87fe565338ca1576083b9e38ad634574e5105cbc
Bazil <bazilkhn@gmail.com>
2024-01-29 23:46:05 -0500

    Add in shadcn libraries

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, Navigate } from "react-router-dom";
-import "./Navbar.css";
 import logo from "../images/icon.png";
 import headerImg from "../images/banner.png";
 import '../App.css';
