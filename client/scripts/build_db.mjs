9e0b3fa0fd3adb5f68d366f25d30813189b6a5b8
Bazil <bazilkhn@gmail.com>
2024-01-29 23:17:49 -0500

    Fix build script

--- client/scripts/build_db.mjs
+++ client/scripts/build_db.mjs
@@ -17,7 +17,7 @@ console.log(EDMTRAIN_KEY)
 async function fetchEdmtrainEvents() {
 
     const start_date = "2024-02-01"
-    const end_date = "2024-02-01"
+    const end_date = "2024-02-02"
     // const end_date = "2024-02-01"
 
     const url = `https://edmtrain.com/api/events?events?startDate=${start_date}&endDate=${end_date}&livestreamInd=false&festivalInd=true&client=${EDMTRAIN_KEY}`
@@ -41,10 +41,10 @@ async function fetchEdmtrainEvents() {
 function transformEventData(event) {
     return {
         event_id: event.id,
-        // link: event.link,
+        edmtrain_link: event.link,
         event_name: event.name,
         event_date: event.date,
-        event_end_date: event.end_date,
+        // event_end_date: event.end_date,
         event_location: event.venue.location,
         event_venue: event.venue.name
         // venue_name: event.venue ? event.venue.name : null,  // Assuming venue object has a name property
